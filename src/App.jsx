5bbc0ee6d42d5fe52ef7f536ba2b8d7e4bfce246
Aakriti-Thapa <thapaaakriti971@gmail.com>
2026-01-15 21:25:20 +0530

    Create multiple pages using react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-
+import { Routes, Route } from 'react-router';
 import { HomePage} from './pages/HomePage';
 import './App.css'
 
 function App() {
   return (
-   <HomePage />
+    <Routes>
+      {/* Shortcuts instead for wrting path="/" React router has prop called index it work the same way as path="/" */}
+      <Route index element={<HomePage />} />
+      <Route path="checkout" element={<div>Projects</div>} />
+    </Routes>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router";
+/* If we just start with a name like this without ./ this means its gonna load it from node modules */
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+);
